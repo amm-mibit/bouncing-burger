e96ceb066c28d7a09007489015786087537e1e4a
Amm-mibit <125179933+amm-mibit@users.noreply.github.com>
2024-01-27 17:47:54 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,43 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite2 = sprites.create(img`
+        . . . . 5 5 5 5 5 5 . . . . . . 
+        . . . . 5 . . . . 5 . . . . . . 
+        . . . . 5 9 . . 9 5 . . . . . . 
+        . . . . 5 . . . . 5 . . . . . . 
+        . . . . 5 . . . . 5 . . . . . . 
+        . . . 5 5 9 9 9 9 5 5 . . . . . 
+        . . . . 5 . . . . 5 . . . . . . 
+        . . . . 2 2 2 2 2 2 . . . . . . 
+        . . 6 6 . . . . . . 6 6 6 . . . 
+        . . 6 . . . . . . 6 . 6 6 . . . 
+        . . 6 . . 6 . . . . 6 6 6 . . . 
+        . . 6 . . . . . . 6 . 6 . . . . 
+        . . 6 . . . . . . . 6 6 . . . . 
+        . . 6 6 6 . . . 6 6 . 6 . . . . 
+        . 6 6 . . . . 6 . 6 6 6 . . . . 
+        . . 6 6 6 6 6 6 6 6 6 6 . . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(mySprite2)
+})
+let mySprite2: Sprite = null
+let mySprite = sprites.create(img`
+    . . . . c c c b b b b b . . . . 
+    . . c c b 4 4 4 4 4 4 b b b . . 
+    . c c 4 4 4 4 4 5 4 4 4 4 b c . 
+    . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+    e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+    e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+    e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+    . e b 4 4 4 4 4 5 4 4 4 4 b e . 
+    8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+    8 7 2 e e e e e e e e e e 2 7 8 
+    e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+    e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+    e b e 8 8 c c 8 8 c c c 8 e b e 
+    e e b e c c e e e e e c e b e e 
+    . e e b b 4 4 4 4 4 4 4 4 e e . 
+    . . . c c c c c e e e e e . . . 
+    `, SpriteKind.Enemy)
+mySprite.vx = 40
+mySprite.vy = 60
+mySprite.setBounceOnWall(true)
